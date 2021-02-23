86bacae4298f80e78ca1f1f4a03b1055223b2094
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-02-23 17:14:33 -0300

    install (typeorm+sqlite3) config database,create migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express from 'express'
+import 'reflect-metadata';
+import express from 'express';
+import './database';
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-    return res.json({ message: "Busca realizada!"});
-})
+    return res.json({ message: "Busca realizada!"})
+});
 
 app.post('/',(req, res) => {
-    return res.json({ message: "Dados Salvos!"});
-})
+    return res.json({ message: "Dados Salvos!"})
+});
 
-app.listen(3333, () => console.log("Server it's Running!!!"))
+app.listen(3333, () => console.log("Server it's Running!!!"));
