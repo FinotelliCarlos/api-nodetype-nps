2e2b2bc9a395fe6fb42750fc0f38188095a0c726
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-02-25 16:35:34 -0300

    create show surveys

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,10 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 
 router.post("/users", userController.create)
+
 router.post("/surveys", surveysController.create)
+router.get("/surveys", surveysController.show)
+
 
 
 export {router};
